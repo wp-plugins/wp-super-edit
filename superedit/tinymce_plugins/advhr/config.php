4a0f78aa5f81dcb0b273520b79f22b7e737b2297
ev3rywh3re <ev3rywh3re@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-02-26 20:43:16 +0000

    add advhr tinymce plugin. For HR tags.
    git-svn-id: https://plugins.svn.wordpress.org/wp-super-edit/trunk@33095 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ superedit/tinymce_plugins/advhr/config.php
@@ -0,0 +1,25 @@
+; <?php die( 'This file should not be used directly' ); ?>
+; -- do not modify the above line for security reasons --
+;
+; WP Super Edit Plugin Configuration file
+;
+; This is a plugin configuration file for WP Super Edit.
+; Each TinyMCE plugin added to WP Super Edit should have similar options.
+;
+
+; WP Super Edit options for this plugin
+
+desc = "Advanced HR"
+notice = "Adds button for manipulating rule lines."
+status = N
+
+; Tiny MCE Buttons provided by this plugin
+
+[advhr]
+desc = "Advanced HR"
+notice = "Button to manipulate rule lines."
+status = N
+row = 0
+position = 0
+separator = N
+plugin = "advhr"
