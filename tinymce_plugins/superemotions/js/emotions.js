a08df83ee52adcfb63a6ed07c9ae62ad985e4021
ev3rywh3re <ev3rywh3re@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-09-21 17:43:49 +0000

    Fix superemotions syntax errors. Transition development for superclass plugin to TinyMCE 3.x.
    git-svn-id: https://plugins.svn.wordpress.org/wp-super-edit/trunk@65757 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tinymce_plugins/superemotions/js/emotions.js
+++ tinymce_plugins/superemotions/js/emotions.js
@@ -9,11 +9,11 @@ var SuperEmotionsDialog = {
 		var ed = tinyMCEPopup.editor, dom = ed.dom;
 
 		tinyMCEPopup.execCommand('mceInsertContent', false, dom.createHTML('img', {
-			src : '../wp-includes/images/smilies/' + file,
-			class: 'superemotionImg mceTemp',
-			alt : ed.getLang(title),
-			title : ed.getLang(title),
-			border : 0
+			'src' : '../wp-includes/images/smilies/' + file,
+			'class' : 'superemotionImg mceTemp',
+			'alt' : ed.getLang(title),
+			'title' : ed.getLang(title),
+			'border' : 0
 		}));
 		
 		tinyMCEPopup.close();
