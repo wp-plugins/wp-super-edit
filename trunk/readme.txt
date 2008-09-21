94694487eee19a3338ddf3c2229e2ad5b86e4558
ev3rywh3re <ev3rywh3re@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-09-21 14:54:22 +0000

    Tag WP Super Edit 2.0.2
    git-svn-id: https://plugins.svn.wordpress.org/wp-super-edit/tags/2.0.1@65751 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/readme.txt
@@ -0,0 +1,64 @@
+=== Plugin Name ===
+Contributors: ev3rywh3re
+Donate link: http://factory.funroe.net/contribute/
+Tags: post, TinyMCE, editor, wysiwyg, buttons, formatting, admin
+Requires at least: 2.6
+Tested up to: 2.7
+Stable tag: 2.0.2
+
+WP Super Edit is designed to give some control over the visual/wysiwyg editor and add some functionality without modifying the Wordpress source code.
+
+== Description ==
+
+WP Super Edit is designed to give some control over the Wordpress visual wysiwyg editor and add some functionality without modifying the Wordpress source code. WP Super edit acts as framework for TinyMCE visual editor plugins and buttons.  
+
+**Features**
+
+* Different modes for editor settings; One editor setting for all users, role based editor settings, and individual user editor settings
+* Drag and Drop interface for arranging the visual editor buttons.
+* Access to built-in editor buttons and functions. 
+* Customized TinyMCE plugins to add additional buttons and features like tables, layers (div tag), emoticons, style attributes, css classes from theme files, search / replace, and more.
+
+For detailed information, visit the **WP Super Edit project** :
+
+* [ http://factory.funroe.net/projects/wp-super-edit/ ](http://factory.funroe.net/projects/wp-super-edit/)
+
+For in depth documentation visit **Using WP Super Edit** :
+
+* [ http://factory.funroe.net/projects/wp-super-edit/using/ ](http://factory.funroe.net/projects/wp-super-edit/using/)
+
+Your feedback is always welcome!
+
+== Installation ==
+
+1. Take the whole whole “wp-super-edit” folder and put it in the wp-content/plugins/ directory of your Web site. How it gets there is up to you (FTP, WebDAV, RSYNC, SCP.. choose your poison!)
+2. Activate the plugin on the Wordpress Plugins administration panel
+3. Click on the Settings option and you will see WP Super Edit in the sub menu.
+4. WP Super Edit should lead you through the final installation steps. Please be patient! WP Super Edit will attempt to save original settings while installing. WP Super Edit will not work until the installation has been completed!
+
+**Upgrade**
+
+**Versions 1.5 or earlier:**
+
+Before you install WP Super Edit, you should deactivate the old version of WP Super Edit and remove the superedit folder from the wp-content/plugins folder of your WordPress installation. Follow the installation instructions for the updated version. The updated version should use the wp-super-edit folder in wp-content/plugins and be more compatible and easier to update in the future. Due to the changes in the WordPress editor your old settings will not be updated for the new version of WP Super Edit.
+
+For more detailed documentation visit **Using WP Super Edit** :
+
+* [ http://factory.funroe.net/projects/wp-super-edit/using/ ](http://factory.funroe.net/projects/wp-super-edit/using/)
+
+
+== Frequently Asked Questions ==
+
+= Can this be used in my language? =
+
+Yes! It may require some work. You can check out the documentation at **Using WP Super Edit** :
+
+* [ http://factory.funroe.net/projects/wp-super-edit/using/ ](http://factory.funroe.net/projects/wp-super-edit/using/)
+
+
+== Screenshots ==
+
+1. WordPress editor with all buttons activated.
+2. WP Super Edit settings.
+3. Drag and drop button settings.
+
---
+++ trunk/wp-super-edit-admin.php
@@ -0,0 +1,358 @@
+<?php
+/**
+* WP Super Edit Administration interface 
+*
+* These functions control the display for the administrative interface. This
+* interface allows drag and drop control for buttons and interactive control for
+* activating TinyMCE plugins. This interface requires a modern browser and 
+* javascript.
+* @package wp-super-edit
+* @subpackage wp-super-edit-admin
+*/
+
+/**
+* WP Super Edit Plugin Folder Scan
+*
+* Scans tinymce_plugin folder for config files with registration commands.
+* @global object $wp_super_edit 
+*/
+function wp_super_edit_plugin_folder_scan() {
+	global $wp_super_edit;
+	
+	$tinymce_plugins = @ dir( $wp_super_edit->tinymce_plugins_path );
+	
+	while( ( $tinymce_plugin = $tinymce_plugins->read() ) !== false) {
+	
+		$tinymce_plugin_path = $wp_super_edit->tinymce_plugins_path . $tinymce_plugin . '/';
+		
+		if ( is_dir( $tinymce_plugin_path ) && is_readable( $tinymce_plugin_path ) ) {
+			if ( $tinymce_plugin{0} == '.' || $tinymce_plugin == '..' ) continue;
+
+			$tinymce_plugin_dir = @ dir( $tinymce_plugin_path );
+			
+			while ( ( $tinymce_plugin_config = $tinymce_plugin_dir->read() ) !== false) {
+			
+				if ( $tinymce_plugin_config == 'config.php' ) {
+					include_once( $tinymce_plugin_path . $tinymce_plugin_config );
+					break;
+				}
+				
+			}
+		}
+	}
+	
+}
+
+/**
+* Set up administration interface
+*
+* Function used by Wordpress to initialize the adminsitrative interface. This function also handles option changes based on user interface.
+* @global object $wp_super_edit 
+*/
+function wp_super_edit_admin_setup() {
+	global $wp_super_edit, $top_submenu;
+				
+	$wp_super_edit_option_page = add_options_page( __('WP Super Edit', 'wp_super_edit'), __('WP Super Edit', 'wp_super_edit'), 5, 'wp-super-edit-admin.php', 'wp_super_edit_admin_page');
+
+    if ( $wp_super_edit->management_mode == 'users' ) {
+		$wp_super_edit_user_page = add_submenu_page('profile.php', __('WP Super Edit', 'wp_super_edit'), __('WP Super Edit', 'wp_super_edit'), 0, 'wp-super-edit-admin.php', 'wp_super_edit_user_page');
+		$top_submenu['profile.php'][]  = array( __('WP Super Edit'), 'edit_posts', 'profile.php?page=wp-super-edit-admin.php' );
+	}
+	
+	if ( strstr( $_GET['page'], 'wp-super-edit-admin' ) != false ) {
+
+		if (  $_REQUEST['wp_super_edit_action'] == 'install' ) {
+			check_admin_referer( 'wp_super_edit_nonce-' . $wp_super_edit->nonce );
+			if ( !current_user_can('manage_options') ) return;
+			include_once( $wp_super_edit->core_path . 'wp-super-edit-defaults.php');
+			wp_super_edit_install_db_tables();
+			wp_super_edit_wordpress_button_defaults();
+			wp_super_edit_plugin_folder_scan();
+			wp_super_edit_set_user_default();
+		}
+		
+		if (  $_REQUEST['wp_super_edit_action'] == 'uninstall' ) {
+			check_admin_referer( 'wp_super_edit_nonce-' . $wp_super_edit->nonce );
+			if ( !current_user_can('manage_options') ) return;
+			$wp_super_edit->uninstall();
+			$wp_super_edit->is_installed = false;
+		}
+		
+		if (  $_REQUEST['wp_super_edit_action'] == 'options' ) {
+			check_admin_referer( 'wp_super_edit_nonce-' . $wp_super_edit->nonce );
+			if ( !current_user_can('manage_options') ) return;
+			$wp_super_edit->do_options();
+		}
+		
+		if (  $_REQUEST['wp_super_edit_action'] == 'plugins' ) {
+			check_admin_referer( 'wp_super_edit_nonce-' . $wp_super_edit->nonce );
+			if ( !current_user_can('manage_options') ) return;
+			$wp_super_edit->do_plugins();
+		}
+		
+		if (  $_REQUEST['wp_super_edit_action'] == 'buttons' ) {
+			check_admin_referer( 'wp_super_edit_nonce-' . $wp_super_edit->nonce );
+			if ( !current_user_can('edit_posts') ) return;
+			$wp_super_edit->do_buttons();
+		}		
+	
+		if ( $wp_super_edit->ui == 'buttons' ) {
+
+			wp_enqueue_script( 'wp-super-edit-ui',  '/wp-content/plugins/wp-super-edit/js/jquery-ui-1.5.2.packed.js', false, '2135' );
+			
+			add_action('admin_footer', 'wp_super_edit_admin_footer');
+		}
+
+		add_action('admin_head', 'wp_super_edit_admin_head');
+
+	}
+}
+
+/**
+* Add javascript and css to the HEAD area
+*
+* Some complex CSS and javascript functions to operate the WP Super Edit advanced interface.
+* @global object $wp_super_edit 
+*/
+function wp_super_edit_admin_head() {
+	global $wp_super_edit;
+?>
+
+	<link rel="stylesheet" href="<?php echo $wp_super_edit->core_uri ?>css/wp_super_edit.css" type="text/css" />
+		
+	<?php if ( $wp_super_edit->is_installed == true ) return; ?>
+	
+	<script type='text/javascript'>
+	/* <![CDATA[ */
+		jQuery(document).ready( function() {
+						
+			jQuery( '#wp_super_edit_install_form' ).hide();
+			jQuery( '#wp_super_edit_install_wait' ).hide();
+
+			jQuery( '#wp_super_edit_install_scanner' ).click( function() {
+			
+				jQuery( '#wp_super_edit_install_scanner' ).fadeOut();
+				jQuery( '#wp_super_edit_install_wait' ).fadeIn();
+
+				jQuery( '#wp_super_edit_null' ).load( 
+					'<?php bloginfo( 'wpurl' ); ?>/wp-includes/js/tinymce/tiny_mce_config.php', 
+					{ scan: 'wp_super_edit_tinymce_scan', uncache: <?php echo rand( 100, 500 ); ?> },
+					function() {
+						jQuery( '#wp_super_edit_install_wait' ).fadeOut();
+						jQuery( '#wp_super_edit_install_form' ).fadeIn();
+					}
+				);
+			} );
+		} );
+	/* ]]> */
+	</script>
+
+
+<?php
+}
+
+
+/**
+* Display user profile WP Super Edit interface
+*
+* Very advanced control interface for TinyMCE buttons and plugins using
+* drag and drop.
+* @global object $wp_super_edit 
+*/
+function wp_super_edit_user_page() {
+	global $wp_super_edit;
+		
+	$updated = false;
+	
+	$wp_super_edit->ui_header();
+	
+	$wp_super_edit->buttons_ui();
+	
+	$wp_super_edit->ui_footer();
+}
+
+/**
+* Display administrative WP Super Edit interface
+*
+* Very advanced control interface for TinyMCE buttons and plugins using
+* drag and drop.
+* @global object $wp_super_edit 
+*/
+function wp_super_edit_admin_page() {
+	global $wp_super_edit;
+		
+	$updated = false;
+	
+	$wp_super_edit->ui_header();
+	
+	if ( !$wp_super_edit->is_installed && $_REQUEST['wp_super_edit_action'] != 'install' ) {
+		$wp_super_edit->install_ui();
+		$wp_super_edit->ui_footer();
+		return;
+	}
+
+	if (  $_REQUEST['wp_super_edit_action'] == 'uninstall' ) {
+		$wp_super_edit->install_ui();
+		$wp_super_edit->ui_footer();
+		return;
+	}
+	
+	$wp_super_edit->admin_menu_ui();
+
+	switch ( $wp_super_edit->ui ) {
+		case 'buttons':
+			$wp_super_edit->buttons_ui();
+			break;
+		case 'plugins':
+			$wp_super_edit->plugins_ui();
+			break;
+		case 'options':
+			$wp_super_edit->options_ui();
+			break;
+		default:
+			$wp_super_edit->options_ui();
+	}
+	
+	$wp_super_edit->ui_footer();
+}
+
+/**
+* Add javascript to the admin footer area
+*
+* Some complex CSS and javascript functions to operate the WP Super Edit advanced interface.
+* @global object $wp_super_edit 
+*/
+function wp_super_edit_admin_footer() {
+	global $wp_super_edit;
+?>
+
+<script type="text/javascript">
+	// <![CDATA[
+
+	// Define custom jQuery namespace to keep away javascript conflicts
+	var wpsuperedit = jQuery.noConflict();
+
+	// Default Variables and Objects
+		
+	function wp_super_edit_button( desc, notice, status, plugin ) {
+		this.desc = desc;
+		this.notice = notice;
+		this.status = status;
+		this.plugin = plugin;
+	  }
+	
+	var data;
+	var button_separators = new Array();
+	var tiny_mce_buttons = new Object();
+	var buttons = new Array();
+	
+	<?php $wp_super_edit->buttons_js_objects(); ?>
+	
+	// Plugin and Button Control Functions
+	
+	function toggleSeparator(button) {
+		wpsuperedit( '#' + button ).toggleClass( 'button_separator' );
+	}
+
+	function getButtonInfo(button) {
+		
+		wpsuperedit( '#wp_super_edit_dialog' ).attr( 'title', tiny_mce_buttons[button].desc );
+		wpsuperedit( '#wp_super_edit_dialog' ).html( '<p>' + tiny_mce_buttons[button].notice + '</p>');
+		wpsuperedit( '#wp_super_edit_dialog' ).removeClass( 'hidden' );
+		
+		wpsuperedit('#wp_super_edit_dialog').dialog({ 
+			resizable: false,
+			modal: true, 
+			overlay: { 
+				opacity: 0.5,
+				background: "black" 
+			},
+			close: function() {
+				wpsuperedit( '#wp_super_edit_dialog' ).addClass( 'hidden' );
+			}
+		});
+		
+		return false;		
+	}
+	
+	function submitButtonConfig() {
+	
+		wpsuperedit('#i_wp_super_edit_row_1').attr('value', wpsuperedit('#row_section_1').sortable('toArray').join(",") );
+		wpsuperedit('#i_wp_super_edit_row_2').attr('value', wpsuperedit('#row_section_2').sortable('toArray').join(",") );
+		wpsuperedit('#i_wp_super_edit_row_3').attr('value', wpsuperedit('#row_section_3').sortable('toArray').join(",") );
+		wpsuperedit('#i_wp_super_edit_row_4').attr('value', wpsuperedit('#row_section_4').sortable('toArray').join(",") );
+		
+		submit_separators = wpsuperedit( '.button_separator' ).map(function() {
+			return wpsuperedit(this).attr('id');
+		}).get().join(",");
+		
+		wpsuperedit('#i_wp_super_edit_separators').attr('value', submit_separators)	
+	}
+
+	wpsuperedit(document).ready(
+		function() {
+			
+			// Drag and Drop Controls
+			wpsuperedit('#row_section_1').sortable(
+				{
+					connectWith: ['#row_section_disabled', '#row_section_2', '#row_section_3', '#row_section_4' ],
+					scroll: true,
+					placeholder: 'sort_placeholder',
+					opacity: 0.7,
+					items: '.button_control',
+					tolerance: 'pointer'
+				}
+			);
+				
+			wpsuperedit('#row_section_2').sortable(
+				{
+					connectWith: ['#row_section_disabled', '#row_section_1', '#row_section_3', '#row_section_4' ],
+					scroll: true,
+					placeholder: 'sort_placeholder',
+					opacity: 0.7,
+					items: '.button_control',
+					tolerance: 'pointer'
+				}
+			);		
+			
+			wpsuperedit('#row_section_3').sortable(
+				{
+					connectWith: ['#row_section_disabled', '#row_section_1', '#row_section_2', '#row_section_4' ],
+					scroll: true,
+					placeholder: 'sort_placeholder',
+					opacity: 0.7,
+					items: '.button_control',
+					tolerance: 'pointer'
+				}
+			);
+			
+			wpsuperedit('#row_section_4').sortable(
+				{
+					connectWith: ['#row_section_disabled', '#row_section_1', '#row_section_2', '#row_section_3' ],
+					scroll: true,
+					placeholder: 'sort_placeholder',
+					opacity: 0.7,
+					items: '.button_control',
+					tolerance: 'pointer'
+				}
+			);
+			
+			wpsuperedit('#row_section_disabled').sortable(
+				{
+					connectWith: ['#row_section_1', '#row_section_2', '#row_section_3', '#row_section_4' ],
+					scroll: true,
+					placeholder: 'sort_placeholder',
+					opacity: 0.7,
+					items: '.button_control',
+					tolerance: 'pointer'
+				}
+			);
+		}
+	);
+
+	// ]]>
+</script> 
+<?php
+}
+// End - Superedit Admin Panel //
+?>
