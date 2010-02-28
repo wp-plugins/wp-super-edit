94fca08496bcd9c53485aca981be877f9044a02a
ev3rywh3re <ev3rywh3re@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-28 22:33:10 +0000

    Adding the WP Super Edit upgrader. This extra plugin will be needed to handle the db changes from yanking out suerclass and superemotions plugins.
    git-svn-id: https://plugins.svn.wordpress.org/wp-super-edit/trunk@211713 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-super-edit-upgrader.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Plugin Name: WP Super Edit Upgrade Utility
+Plugin URI: http://funroe.net/projects/super-edit/
+Description: Utility for upgrading WP Super Edit to latest version
+Author: Jess Planck
+Version: 2.2.1
+Author URI: http://funroe.net
+
+Copyright (c) Jess Planck (http://funroe.net)
+WP Super Edit is released under the GNU General Public
+License: http://www.gnu.org/licenses/gpl.txt
+
+This is a WordPress plugin (http://wordpress.org). WordPress is
+free software; you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free
+Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+For a copy of the GNU General Public License, write to:
+
+Free Software Foundation, Inc.
+59 Temple Place, Suite 330
+Boston, MA  02111-1307
+USA
+
+You can also view a copy of the HTML version of the GNU General
+Public License at http://www.gnu.org/copyleft/gpl.html
+*/
+
+
+
+?>
